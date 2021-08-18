7c7e30de24620f3ce83bef1dee42f4f28d0ee062
losuna <losuna@bofifederalbank.com>
2021-08-18 17:03:30 -0500

    fixing unosquare test

--- test/unosquare.spec.js
+++ test/unosquare.spec.js
@@ -20,15 +20,15 @@ module.exports = {
         unosquare.expect.element('//*[@class="subtitle"]').text.to.contain('AGILE SOFTWARE DEVELOPMENT');
         unosquare.useXpath().click("//a[@href='/PracticeAreas']");
         unosquare.expect.element('//*[@class="subtitle"]').text.to.contain('PARTNER WITH EXPERIENCED SOFTWARE DEVELOPMENT EXPERTS');
+
         unosquare.useXpath().click("//a[@href='/Industries']");
+
         unosquare.expect.element('//*[@class="subtitle"]').text.to.contain('OUR EXPERTISE');
-        unosquare.waitForElementVisible('//*[@class="header-title"]', 10000);
-        //unosquare.expect.elements('//a[@class="link-blue"]').count.to.equal(3);
-        unosquare.useXPath().click("//a[@href='/Industries/Bfsi']");
-        //unosquare.useXpath().click("//a[@href='/Industries/Bfsi']");
-        //unosquare.expect.element('//*[@class="header-title"]').text.to.contain('DIGITAL TRANSFORMATION IN HIGHLY REGULATED INDUSTRIES');
 
-        
+        //unosquare.expect.elements("css selector","a[href = '/Industries/Bfsi']").count.to.equal(2);
 
+        unosquare.expect.elements("a[href = '/Industries/Bfsi']").count.to.equal(3);
+  
+   
     }
 }
